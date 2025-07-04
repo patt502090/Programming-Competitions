42f0124a6f2d3cb7b0409c3f166e1db81d482520
Phodcharaphon <patt502090@hotmail.com>
2025-07-04 13:38:42 +0700

    Refactor dragon problem solver to enhance mathematical utility functions

---
+++ A. Is It a Cat.py
@@ -0,0 +1,19 @@
+for _ in range(int(input())):
+    n=int(input())
+    a=(input())
+    a=a.lower()
+    b=''
+    i=0
+    while i<n:
+        j=i
+        b+=a[i]
+        while j<n:
+            if a[j]!=a[i]:
+                break
+            else:
+                j+=1
+        i=j
+    if b=='meow':
+        print("YES")
+    else:
+        print("NO")
