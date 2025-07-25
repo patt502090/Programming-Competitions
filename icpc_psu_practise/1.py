81b8643da5adbd18f8de5131534baf16f5b79583
Phodcharaphon <patt502090@hotmail.com>
2025-07-25 22:40:28 +0700

    Add initial implementations for problem-solving scripts and test cases

---
+++ icpc_psu_practise/1.py
@@ -0,0 +1,3 @@
+for i in range (int (input () ) ) :
+    lst = [int (x) for x in input () .split () ]
+    print (min (lst), max (lst) )
---
+++ icpc_psu_practise/3.py
@@ -0,0 +1,2 @@
+lst = list(input().split())
+print(lst[0], lst[-1])
