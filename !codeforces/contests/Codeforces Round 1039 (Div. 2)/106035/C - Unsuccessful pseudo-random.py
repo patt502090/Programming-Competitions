01c3a4e69af22acda585de27c2a0d097011ca6d6
patt502090 <100224660+patt502090@users.noreply.github.com>
2025-08-22 12:45:20 +0000

    autopep8 action fixes [skip ci]

--- !codeforces/contests/Codeforces Round 1039 (Div. 2)/106035/C - Unsuccessful pseudo-random.py
+++ !codeforces/contests/Codeforces Round 1039 (Div. 2)/106035/C - Unsuccessful pseudo-random.py
@@ -1,9 +1,10 @@
 import math
 
+
 def ones_in_r(n, m, k):
-    d = abs(n - m)             
+    d = abs(n - m)
     combs = [math.comb(k, i) for i in range(k + 1)]
-    max_bitlen = max(c.bit_length() for c in combs) 
+    max_bitlen = max(c.bit_length() for c in combs)
 
     if d >= max_bitlen:
         return sum(c.bit_count() for c in combs)
@@ -12,6 +13,7 @@ def ones_in_r(n, m, k):
     for i, c in enumerate(combs):
         S += c << (d * i)
     return S.bit_count()
+
 
 n, m, k = map(int, input().split())
 print(ones_in_r(n, m, k))
