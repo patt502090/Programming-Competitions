6c81a65c84c05bb0cbb5868aca537b346ed250e0
Phodcharaphon Sukonsakun <patt502090@hotmail.com>
2025-08-29 19:17:46 +0700

    upload test case

--- !programmingTH/16 Ptice.py
+++ !programmingTH/16 Ptice.py
@@ -1,18 +1,18 @@
-N = int(input())
-Adrian = ['A', 'B', 'C']*N
-Bruno = ['B', 'A', 'B', 'C']*N
-Goran = ['C', 'C', 'A', 'A', 'B', 'B']*N
+n = int(input())
+Adrian = ['A', 'B', 'C']*n
+Bruno = ['B', 'A', 'B', 'C']*n
+Goran = ['C', 'C', 'A', 'A', 'B', 'B']*n
 scores = {'Adrian':0, 'Bruno':0, 'Goran':0}
 key = input()
-for i in range(N):
+for i in range(n):
     if (Adrian[i] == key[i]):
         scores['Adrian'] += 1
     if (Bruno[i] == key[i]):
         scores['Bruno'] += 1
     if (Goran[i] == key[i]):
         scores['Goran'] += 1
-max_score = max(scores['Adrian'], scores['Bruno'], scores['Goran'])
-print(max_score)
-for name in scores:
-    if (scores[name] == max_score):
+tmp = max(scores.values())
+print(tmp)
+for name, score in scores.items():
+    if score == tmp:
         print(name)
