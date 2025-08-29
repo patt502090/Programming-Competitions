ddaafdcbd356561af169a8981807bb52dad0ee2b
Phodcharaphon Sukonsakun <patt502090@hotmail.com>
2025-08-29 19:14:04 +0700

    add 16 ptice progth

--- !programmingTH/16 Ptice.py
+++ !programmingTH/16 Ptice.py
@@ -1,95 +1,18 @@
-ImportType = InputType = ConstType = 1
-DecoratorType = FunctinoType = 1
-
-if ImportType:
-    import os
-    import sys
-    import random
-    import threading
-    from copy import deepcopy
-    from decimal import Decimal, getcontext
-    from random import randint, choice, shuffle
-    from types import GeneratorType
-    from functools import lru_cache, reduce
-    from bisect import bisect_left, bisect_right
-    from collections import Counter, defaultdict, deque
-    from itertools import accumulate, combinations, permutations
-    from heapq import heapify, heappop, heappush, heappushpop
-    from typing import Generic, Iterable, Iterator, TypeVar, Union, List
-    from string import ascii_lowercase, ascii_uppercase, digits
-    from math import ceil, floor, sqrt, pi, factorial, gcd, log, log10, log2, inf
-    from sys import stdin, stdout, setrecursionlimit
-    from string import (
-        ascii_lowercase,
-        ascii_uppercase,
-        digits,
-        punctuation,
-        printable,
-        whitespace,
-    )
-
-
-if InputType:
-    def input(): return sys.stdin.readline().rstrip("\r\n")
-    def I(): return input()
-    def II(): return int(input())
-    def MII(): return map(int, input().split())
-    def LI(): return list(input())
-    def LII(): return list(map(int, input().split()))
-    def GMI(): return map(lambda x: int(x) - 1, input().split())
-    def LGMI(): return list(map(lambda x: int(x) - 1, input().split()))
-
-if FunctinoType:
-
-    class Math:
-        __slots__ = ["mod", "l", "fact", "inv"]
-
-        def __init__(self):
-            self.mod = mod = 10**9 + 7
-            self.l = l = 3 * 10**5 + 5
-            self.fact = fact = [1] * (l + 1)
-            self.inv = inv = [1] * (l + 1)
-            for i in range(1, l + 1):
-                fact[i] = fact[i - 1] * i % mod
-            inv[l] = pow(fact[l], mod - 2, mod)
-            for i in range(l - 1, -1, -1):
-                inv[i] = inv[i + 1] * (i + 1) % mod
-
-        def comb(self, n: int, r: int):  # (Combination) CNR เลขจัดหมู่
-            return (self.fact[n] * self.inv[r] %
-                    self.mod * self.inv[n - r] %
-                    self.mod if n >= r >= 0 else 0)
-
-        def perm(self, n: int, r: int):  # (Permutation) PNR เลขเรียงสับเปลี่ยน
-            return self.fact[n] * self.inv[n -
-                                           r] % self.mod if n >= r >= 0 else 0
-
-        # math_obj = Math()
-        # combination = math_obj.comb(5, 2)  # คำนวณค่า C(5, 2) = 10
-        # permutation = math_obj.perm(5, 2)  # คำนวณค่า P(5, 2) = 20
-
-if ConstType:
-    MOD1, MOD9 = 10**9 + 7, 998244353
-    RD = random.randint(MOD1, MOD1 << 1)
-    Direction4 = [(0, 1), (0, -1), (1, 0), (-1, 0)]  # ->, <-, v, ^
-    Direction8 = [
-        (0, 1),
-        (0, -1),
-        (1, 0),
-        (-1, 0),
-        (1, 1),
-        (1, -1),
-        (-1, 1),
-        (-1, -1),
-    ]  # ->, <-, v, ^, ↘, ↙, ↗, ↖
-    Y, N = "Yes", "No"
-    A, B = "Alice", "Bob"
-
-
-def solve():
-
-
-if __name__ == "__main__":
-    TEST = II()
-    for _ in range(TEST):
-        solve()
+N = int(input())
+Adrian = ['A', 'B', 'C']*N
+Bruno = ['B', 'A', 'B', 'C']*N
+Goran = ['C', 'C', 'A', 'A', 'B', 'B']*N
+scores = {'Adrian':0, 'Bruno':0, 'Goran':0}
+key = input()
+for i in range(N):
+    if (Adrian[i] == key[i]):
+        scores['Adrian'] += 1
+    if (Bruno[i] == key[i]):
+        scores['Bruno'] += 1
+    if (Goran[i] == key[i]):
+        scores['Goran'] += 1
+max_score = max(scores['Adrian'], scores['Bruno'], scores['Goran'])
+print(max_score)
+for name in scores:
+    if (scores[name] == max_score):
+        print(name)
